eade31430404f32614a11e44dc3d3b7dbb850eb5
Joerg Bellmann <joerg.bellmann@googlemail.com>
2022-12-13 13:34:36 +0100

    Try out feature branch workflow

---
+++ src/test/java/org/ehrbase/fhirbridge/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.ehrbase.fhirbridge;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+  @Test
+  void simple() {
+    Assertions.assertTrue(true);
+  }
+
+}
