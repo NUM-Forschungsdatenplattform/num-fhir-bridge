abc3940cc37bb4ccc6797029bfd609ff7172ec5f
rabeaM <rabea.mueller@sva.de>
2024-10-08 11:29:34 +0200

    fix test imports

--- src/main/java/org/ehrbase/fhirbridge/fhir/bundle/converter/MibiKulturBundleConverter.java
+++ src/main/java/org/ehrbase/fhirbridge/fhir/bundle/converter/MibiKulturBundleConverter.java
@@ -2,10 +2,12 @@ package org.ehrbase.fhirbridge.fhir.bundle.converter;
 
 import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 import org.ehrbase.fhirbridge.fhir.common.Profile;
-import org.hl7.fhir.r4.model.*;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.Reference;
 import org.springframework.lang.NonNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
